24b1c5fcb3f671d32c28a191e9869ce94067837a
Alif Prasetyo Aji <alifprasetyoaji97@gmail.com>
2022-02-05 07:30:59 +0700

    sound notificaiton

--- routes/data_handphone.route.js
+++ routes/data_handphone.route.js
@@ -4,11 +4,11 @@ const { authJwt } = require("../middleware")
 module.exports = function(app) {
 	
 	app.get('/input',[authJwt.verifyToken],controller.formInput)
-	app.post('/api/create',controller.create)
-	app.post('/api/update',controller.update)
+	app.post('/api/create',[authJwt.verifyToken],controller.create)
+	app.post('/api/update',[authJwt.verifyToken],controller.update)
 	app.get('/output',[authJwt.verifyToken],controller.formOutput)
-	app.get('/api/generateNumber',controller.generateNumber)
-	app.get('/api/getId/:id',controller.getId)
+	app.get('/api/generateNumber',[authJwt.verifyToken],controller.generateNumber)
+	app.get('/api/getId/:id',[authJwt.verifyToken],controller.getId)
 	app.post('/api/delete',[authJwt.verifyToken],controller.delete)
 
 	app.get('/',[authJwt.verifyTokenLogin])
